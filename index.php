93c232f36e6d0e9f451d6c6ed2d04db96fc571ec
Briam <chimuelokaky@hotmail.com>
2024-08-16 17:44:52 -0600

    Creacion de index con estructura html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Briam Ernesto Navarro Menjivar </title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css"/>
+        <script src = "js/jquery-3.7.1.min.js"></script>
+        <script src = "js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
