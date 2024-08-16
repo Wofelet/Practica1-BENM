287135765895216daa2eb5c6f8d5b9b98df3bc66
Briam <85311268+Wofelet@users.noreply.github.com>
2024-08-16 17:47:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <title> Briam Ernesto Navarro Menjivar </title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
         <link rel="stylesheet" href="css/style.css"/>
-        <script src = "js/jquery-3.7.1.min.js"></script>
-        <script src = "js/bootstrap.bundle.min.js"></script>
+        <script src = "js/jquery-3.7.1.min.js"> </script>
+        <script src = "js/bootstrap.bundle.min.js"> </script>
     </head>
     <body>
 
